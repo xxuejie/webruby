a750a514ca2d78d8e55f684e09d7fc701c58ff88
Daniel Rodrí­guez <sadasant@gmail.com>
2014-05-13 18:16:35 -0430

    This is the correct ussage of mrb_dump_irep_binary for the latest mruby used by webruby

--- driver/driver.c
+++ driver/driver.c
@@ -60,19 +60,17 @@ int webruby_internal_compile(mrb_state* mrb, const char *file_name, const char *
 	mrbc_context *cxt = mrbc_context_new(mrb);
 	cxt->no_exec = 1;
 
-    int index = mrb->irep_len;
-
-	mrb_load_string_cxt(mrb, code, cxt);
+	mrb_value result = mrb_load_string_cxt(mrb, code, cxt);
 
     mrbc_context_free(mrb, cxt);
 
     FILE* file = fopen(file_name, "wb");
 
-	mrb_dump_irep_binary(mrb, 1, 0, file);
+	mrb_dump_irep_binary(mrb, mrb_proc_ptr(result)->body.irep, 0, file);
 
     fclose(file);
 
-	return 1;
+	return 1; 
 }
 
 int webruby_internal_setup(mrb_state* mrb)
